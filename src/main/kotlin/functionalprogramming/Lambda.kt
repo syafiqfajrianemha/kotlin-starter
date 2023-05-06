b4ea93be1d74fc8f91482bcedcb99082a49a4cc7
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-05-06 10:49:47 +0700

    belajar materi tentang: Lambda

---
+++ src/main/kotlin/functionalprogramming/Lambda.kt
@@ -0,0 +1,24 @@
+package functionalprogramming
+
+/*
+lambda sangat berguna karena dapat membuat penulisan kode menjadi lebih mudah dan sederhana.
+ */
+
+// contoh deklarasi dari lambda
+// ditandai dengan sepasang kurung kurawal
+val message = {println("Hello From Lambda")}
+
+// mendeklarasi lambda agar dapat mengembalikan nilai
+val nameLength = {name: String -> name.length}
+
+// menambahkan sebuah parameter pada fungsi lambda
+val sayHello = {name: String -> println("Hello $name")}
+
+fun main() {
+    message()
+
+    val nameLength = nameLength("emha")
+    println(nameLength)
+    
+    sayHello("Syafiq")
+}
