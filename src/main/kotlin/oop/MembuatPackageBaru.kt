4184ae037f89cd91a2e336c9155003dd211b1229
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-05-12 11:05:44 +0700

    belajar materi tentang: Membuat Package Baru

---
+++ src/main/kotlin/oop/MembuatPackageBaru.kt
@@ -0,0 +1,14 @@
+package oop
+
+/*import com.dicoding.oop.utils.PI
+import com.dicoding.oop.utils.factorial
+import com.dicoding.oop.utils.pow
+import com.dicoding.oop.utils.sayHello*/
+import com.dicoding.oop.utils.*
+
+fun main() {
+    sayHello()
+    println(factorial(4.0))
+    println(pow(3.0, 2.0))
+    println(PI)
+}
